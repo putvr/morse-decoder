28b4417f12718731009773a3e9f1f7c031a221bf
Roman Putilov <Putilov.Roman@gmail.com>
2022-01-25 17:40:21 +0500

    Solution

--- src/index.js
+++ src/index.js
@@ -37,8 +37,30 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(expr) {   
+    let str = '';
+
+    for(let i=0; i < expr.length; i+=10) {           
+        let d = expr.slice(i, i+10);
+
+        if (d === '**********') {
+            str += ' ';
+            continue;
+        }
+        
+        let sym = '';
+        for(let j=0; j < 10; j+=2) {
+            const b = `${d[j]}${d[j+1]}`;
+            switch(b) {
+                case '00': break; 
+                case '10': sym += '.'; break;
+                case '11': sym += '-'; break;
+            }
+        }
+        
+        str +=  MORSE_TABLE[sym];
+    } 
+    return str;
 }
 
 module.exports = {
